6ae07654551f8bf5967b8e6de461468f65a86941
Henrick Deschamps <hdeschamps@toulouse.viveris.com>
2015-02-13 15:23:03 +0100

    ZC buffer max size ajustement.

--- include/zc_buffer.h
+++ include/zc_buffer.h
@@ -14,11 +14,33 @@
 #include "trailer.h"
 #include "constants.h"
 
-#define ZC_BUFFER_MAX_SIZE ((RLE_START_MANDATORY_HEADER_SIZE + RLE_PROTO_TYPE_FIELD_SIZE_UNCOMP) +	\
-		(RLE_MAX_SEQ_NO * RLE_CONT_HEADER_SIZE) +	\
-	       	RLE_END_HEADER_SIZE +				\
-		RLE_CRC32_FIELD_SIZE +				\
+/**
+ * This define compute the size of the zc buffer, based on the hard coded values of the differents
+ * headers, trailers and pointers sizes.
+ */
+#define ZC_BUFFER_MAX_SIZE_PACKED (((RLE_START_MANDATORY_HEADER_SIZE) + \
+      (RLE_PROTO_TYPE_FIELD_SIZE_UNCOMP)) +	\
+    ((RLE_MAX_SEQ_NO - 2) * (RLE_CONT_HEADER_SIZE)) +	\
+	  (RLE_END_HEADER_SIZE) +	\
+		(RLE_CRC32_FIELD_SIZE) + \
 		(RLE_MAX_SEQ_NO * sizeof(struct zc_ptrs_data)))
+
+/**
+ * This define ajusts the size of the zc buffer, taking account of the architecture alignement
+ * in the structures. 
+ *
+ *     1 x ( Max start header size + alignement + 2 x zc pointers )
+ * + 254 x ( Continue header size  + alignement + 2 x zc pointers )
+ * +   1 x ( End header size       + alignement + 2 x zc pointers )
+ * +   1 x ( Max trailer size                                     )
+ * ----------------------------------------------------------------
+ * = 3080 Octets (arch. 32-bits) / 6148 Octets (arch. 64-bits)
+ *
+ */
+#define ZC_BUFFER_MAX_SIZE ((1 * (int) (sizeof(struct zc_rle_header_complete_w_ptype))) + \
+    (((RLE_MAX_SEQ_NO) - 2) * (int) (sizeof(struct zc_rle_header_cont_end))) + \
+    (1 * (int) (sizeof(struct zc_rle_header_cont_end))) + \
+    (1 * (int) (sizeof(struct zc_rle_trailer))))
 
 /**
  * Structure of pair or pointers
