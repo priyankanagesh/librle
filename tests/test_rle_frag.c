3e538b106304d3c083dc2e1172bed72dce76367f
Henrick Deschamps <hdeschamps@toulouse.viveris.com>
2015-05-20 14:54:40 +0200

    Adding a fragmentation test with real world burst sizes.

--- tests/test_rle_frag.c
+++ tests/test_rle_frag.c
@@ -440,21 +440,19 @@ enum boolean test_frag_real_world(void)
 	};
 
 	const size_t sdu_lengths[] = { 100, 1500 };
-	const size_t *sdu_length;
+	size_t sdu_lengths_it;
 
-	for (sdu_length = sdu_lengths;
-	     sdu_length < sdu_lengths + sizeof(sdu_lengths) / sizeof(size_t);
-	     ++sdu_length) {
+	for (sdu_lengths_it = 0; sdu_lengths_it < sizeof(sdu_lengths) / sizeof *(sdu_lengths); 
+			++sdu_lengths_it) {
+		const size_t sdu_length = sdu_lengths[sdu_lengths_it];
 		const size_t burst_sizes[] =
 		{ 14, 24, 38, 51, 55, 59, 62, 69, 84, 85, 93, 96, 100, 115, 123, 130, 144, 170, 175,
 		  188, 264, 298, 355, 400, 438, 444, 539, 599 };
-		const size_t *burst_size;
-		for (burst_size = burst_sizes;
-		     burst_size < burst_sizes + sizeof(burst_sizes) / sizeof(size_t);
-		     ++burst_size) {
-			const enum boolean ret =
-			        test_frag(protocol_type, conf, *sdu_length, *burst_size,
-			                  frag_id);
+		size_t burst_sizes_it;
+		for (burst_sizes_it = 0; burst_sizes_it < sizeof(burst_sizes) / sizeof *(burst_sizes); 
+				++burst_sizes_it) {
+			const size_t burst_size = burst_sizes[burst_sizes_it];
+			const enum boolean ret = test_frag(protocol_type, conf, sdu_length, burst_size, frag_id);
 			if (ret == BOOL_FALSE) {
 				/* Only one fail means the encap test fail. */
 				output = BOOL_FALSE;
