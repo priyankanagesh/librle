64637bb38e59d88ecf93d745b405e91aff50792f
Didier Barvaux <didier.barvaux@toulouse.viveris.com>
2017-04-27 10:27:33 +0200

    change license to GPLv2

--- tests/test_rle_pack.c
+++ tests/test_rle_pack.c
@@ -1,3 +1,23 @@
+/*
+ * librle implements the Return Link Encapsulation (RLE) protocol
+ *
+ * Copyright (C) 2015-2016, Thales Alenia Space France - All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 /**
  * @file   test_rle_pack.c
  * @brief  Body file used for the packing tests.
